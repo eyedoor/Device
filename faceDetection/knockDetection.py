85f8f818eb5fbf65d18ce69824ecf832beb1d067
jacob3378 <jwk6gc@mail.missouri.edu>
2019-05-05 13:21:35 -0500

    Add files via upload

--- faceDetection/knockDetection.py
+++ faceDetection/knockDetection.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import sys
 
 #GPIO SETUP
 channel = 17
@@ -11,9 +12,10 @@ GPIO.setup(channel, GPIO.IN)
 def callback(channel):
     if GPIO.input(channel):
         print("Knock Detected!")
+        sys.stdout.flush()
         os.system('python3 faceDetection.py')
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=5000)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=10000)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 
 # infinite loop
